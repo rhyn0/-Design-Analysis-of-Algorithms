b5e3c750ce2402a3dc177a90556000e9a08e7bcb
rhyn0 <ryan.ozawa21@gmail.com>
2019-10-07 13:51:21 -0700

    outline assign 3

---
+++ Inversions.java
@@ -0,0 +1,16 @@
+
+
+public class Inversions{
+
+  public static int invCounter(int[] arr){
+    return 0;
+  }
+
+  public static int mSort(int[] arr, int left, int right){
+    return 0;
+  }
+
+  public static int merge(int[] arr, int left, int m, int right){
+    return 0;
+  }
+}
