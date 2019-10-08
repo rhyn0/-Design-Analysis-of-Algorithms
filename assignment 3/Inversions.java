ed1e64f21fc13d333d1d7dd669b0993875eac39b
rhyn0 <ryan.ozawa21@gmail.com>
2019-10-08 16:17:42 -0700

    finish assign 3

--- assignment 3/Inversions.java
+++ assignment 3/Inversions.java
@@ -1,15 +1,21 @@
-
+import java.util.ArrayList;
 
 public class Inversions{
 
   public static int invCounter(int[] arr){
     int catcher = 0;
-    catcher += mSort(arr, 0, arr.length/2);
-    catcher += mSort(arr, (arr.length/2) + 1, arr.length - 1);
-    return catcher + merge(arr, 0, arr.length/2, arr.length - 1);
+    int middle = (arr.length - 1)/2;
+    ArrayList<Integer> mine = new ArrayList<Integer>(arr.length);
+    for(int i = 0; i < arr.length; ++i){
+      mine.add(arr[i]);
+    }
+
+    catcher += mSort(mine, 0, middle);
+    catcher += mSort(mine, middle + 1, arr.length - 1);
+    return catcher + merge(mine, 0, middle, arr.length - 1);
   }
 
-  public static int mSort(int[] arr, int left, int right){
+  public static int mSort(ArrayList<Integer> arr, int left, int right){
     int catcher = 0;
     if (left < right){
       catcher += mSort(arr, left, (left + right)/2);
@@ -19,44 +25,42 @@ public class Inversions{
     return catcher;
   }
 
-  public static int merge(int[] arr, int left, int m, int right){
+  public static int merge(ArrayList<Integer> arr, int left, int m, int right){
     int ret = 0;
     int size1 = m - left + 1;
     int size2 = right - m;
 
-    int[] L = new int[size1];
-    int[] R = new int[size2];
+    int L[] = new int[size1];
+    int R[] = new int[size2];
 
     for(int i = 0; i < size1; ++i)
-      L[i] = arr[left + i];
+      L[i] = arr.get(left + i);
     for (int j = 0; j < size2; ++j)
-      R[j] = arr[m + j];
-
+      R[j] = arr.get(m + 1 + j);
     int i = 0, j = 0;
     int k = left;
     while (i < size1 && j < size2){
       if (L[i] < R[j]){
-        arr[k] = L[i];
+        arr.set(k, L[i]);
         ++i;
       }
       else{
-        arr[k] = R[j];
+        arr.set(k, R[j]);
         ++j;
         ret += (size1 - i);
       }
       ++k;
     }
     while (i < size1){
-      arr[k] = L[i];
+      arr.set(k, L[i]);
       ++i;
       ++k;
     }
     while (j < size2){
-      arr[k] = R[j];
+      arr.set(k, R[j]);
       ++j;
       ++k;
     }
-
     return ret;
   }
 }
