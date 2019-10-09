f208dd7c022b6f68c06dd31ed8df9dd11d644980
rhyn0 <ryan.ozawa21@gmail.com>
2019-10-08 17:14:48 -0700

    finish assign 3

---
+++ assignment 3/Inversions.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Inversions{
+
+  public static int invCounter(int[] arr){
+    int catcher = 0;
+    int middle = (arr.length - 1)/2;
+    ArrayList<Integer> mine = new ArrayList<Integer>(arr.length);
+    for(int i = 0; i < arr.length; ++i){           // turn primitive array into ArrayList to work in passing by reference
+      mine.add(arr[i]);
+    }
+
+    catcher += mSort(mine, 0, middle);
+    catcher += mSort(mine, middle + 1, arr.length - 1);
+    return catcher + merge(mine, 0, middle, arr.length - 1);
+  }
+
+  public static int mSort(ArrayList<Integer> arr, int left, int right){
+    int catcher = 0;
+    if (left < right){
+      catcher += mSort(arr, left, (left + right)/2);
+      catcher += mSort(arr, ((left + right)/2) + 1, right);
+      return catcher + merge(arr, left, (left + right)/2, right);
+    }
+    return catcher;
+  }
+
+  public static int merge(ArrayList<Integer> arr, int left, int m, int right){
+    int ret = 0;
+    int size1 = m - left + 1;
+    int size2 = right - m;
+
+    int L[] = new int[size1];     // make temporary arrays to help sort
+    int R[] = new int[size2];
+
+    if (left == right)            // fast out for array of size 1
+      return 0;
+
+    for(int i = 0; i < size1; ++i) // divide full array into halves
+      L[i] = arr.get(left + i);
+    for (int j = 0; j < size2; ++j)
+      R[j] = arr.get(m + 1 + j);
+
+    int i = 0, j = 0;
+    int k = left;
+    while (i < size1 && j < size2){
+      if (L[i] < R[j]){           // if this case, then the elements were in proper order. COMPARISON
+        arr.set(k, L[i]);
+        ++i;
+      }
+      else{                       // element that is in wrong order, needs to move past the remaining
+        arr.set(k, R[j]);         // left array to get into proper position
+        ++j;
+        ret += (size1 - i);
+      }
+      ++k;
+    }
+    while (i < size1){
+      arr.set(k, L[i]);
+      ++i;
+      ++k;
+    }                             // add remaining elements from one arrays to sorted array
+    while (j < size2){
+      arr.set(k, R[j]);
+      ++j;
+      ++k;
+    }
+    return ret;
+  }
+}
---
+++ assignment 3/InversionsTest.java
@@ -0,0 +1,20 @@
+
+
+public class InversionsTest{
+
+  public static void main(String[] args){
+    int[] arr = new int[1];
+    arr[0] = 2;
+    // arr[1] = 3;
+    // arr[2] = 8;
+    // arr[3] = 6;
+    // arr[4] = 1;
+    int check = Inversions.invCounter(arr);
+    if (check != 6){
+      System.out.println("Error, didn't get right answer. Instead got " + check);
+    }
+    else{
+      System.out.println("Correct!");
+    }
+  }
+}
