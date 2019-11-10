c54ac6d48f456d16dbc989f548cb4a40af631b3b
rhyn0 <ryan.ozawa21@gmail.com>
2019-11-09 20:48:44 -0800

    near completion of assign 7

--- EditDistance.java
+++ EditDistance.java
@@ -13,6 +13,9 @@ public class EditDistance{
     String x = input.nextLine();
     String y = input.nextLine();
     int[][] table = new int[y.length() + 1][x.length() + 1];
+    ArrayList<Integer> answerKey = new ArrayList<>();
+    for(int i = 0; i < Math.max(x.length(), y.length()); ++i)
+      answerKey.add(0);
 
     for(int i = 0; i <= y.length(); ++i){ //iteration to fill the table
       for(int j = 0; j <= x.length(); ++j){
@@ -20,15 +23,53 @@ public class EditDistance{
           table[i][j] = 2 * j;
         else if (j == 0)
           table[i][j] = 2 * i;
-        else if (y[i - 1] == x[j - 1])
+        else if (y.charAt(i - 1) == x.charAt(j - 1))
           table[i][j] = table[i - 1][j - 1];
         else
-          table[i][j] = Math.min(1 + table[i - 1][j - 1], 2 + table[i - 1][j], 2 + table[i][j - 1]);
+          table[i][j] = Math.min(1 + table[i - 1][j - 1], Math.min(2 + table[i - 1][j], 2 + table[i][j - 1]));
       }
     }
 
     //traceback
+    int posI = y.length(), posJ = x.length();
+    for(int i = answerKey.size() - 1; i >= 0; --i){
+      if (table[posI][posJ] == table[posI - 1][posJ - 1]){
+        answerKey.set(i, 0);
+        --posI;
+        --posJ;
+      }
+      else if (table[posI][posJ] == (table[posI - 1][posJ - 1] - 1)){
+        answerKey.set(i, 1);
+        --posI;
+        --posJ;
+      }
+      else if(table[posI][posJ] == (table[posI - 1][posJ] - 2)){
+        answerKey.set(i,2);
+        --posI;
+      }
+      else{
+        answerKey.set(i,2);
+        --posJ;
+      }
+      if (posI == 0){
+        --i;
+        while(i >= 0){
+          answerKey.set(i, 2);
+          --i;
+        }
+        break;
+      }
+      if (posJ == 0){
+        --i;
+        while(i >= 0){
+          answerKey.set(i, 2);
+          --i;
+        }
+        break;
+      }
+    }
 
+    traceback(x, y, answerKey, table[y.length()][x.length()]);
   }
 
   public static Scanner readfile(String filename){
@@ -44,7 +85,27 @@ public class EditDistance{
     return sc;
   }
 
-  public void traceback(String x, String y, ArrayList<Integer> answer, int minEdit){
-
+  public static void traceback(String x, String y, ArrayList<Integer> answer, int minEdit){
+    boolean m = (x.length() >= y.length() ? true : false);
+    int lessP = 0;
+    System.out.println("Edit distance = " + minEdit);
+    for(int i = 0; i < answer.size(); ++i){
+      if(m){
+        if (answer.get(i) == 0 || answer.get(i) == 1){
+          System.out.println(x.charAt(i) + " " + y.charAt(lessP) + " " + answer.get(i));
+          ++lessP;
+        }
+        else
+          System.out.println(x.charAt(i) + " - 2");
+      }
+      else{
+        if (answer.get(i) == 0 || answer.get(i) == 1){
+          System.out.println(x.charAt(lessP) + " " + y.charAt(i) + " " + answer.get(i));
+          ++lessP;
+        }
+        else
+          System.out.println("- " + y.charAt(i) + " 2");
+      }
+    }
   }
 }
