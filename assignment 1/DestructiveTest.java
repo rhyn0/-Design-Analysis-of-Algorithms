f6f09e5d234f5df56e5b79bf0d66ed9a1d346b6a
Ryan Ozawa <ryan.ozawa21@gmail.com>
2021-07-23 12:33:12 -0700

    add .gitignore, adding more documentation on assignment 1

--- assignment 1/DestructiveTest.java
+++ assignment 1/DestructiveTest.java
@@ -5,6 +5,18 @@ public class DestructiveTest {
 
   public DestructiveTest(){}
 
+  /**
+   * The situation is as follow, the company is testing whether a newly created device will break at a height.
+   * Incrementally go up the rungs of a ladder with number of rungs equal to height.
+   * There are only 3 devices created at this time, so don't break more than 2 of them.
+   * And the company's time is precious, so minimize the number of drops.
+   * Return an array of integers corresponding to the following:
+   *  1. height of ladder
+   *  2. safest rung on the ladder
+   *  3. Highest safe rung checked
+   *  4. Number of drops performed
+   *  5. Size of incrementing range
+   */
   public ArrayList<Integer> findHighestSafeRung(int height, int safest){
     ArrayList<Integer> ret = new ArrayList<Integer>();
 
@@ -20,21 +32,32 @@ public class DestructiveTest {
     drops = highCheck = 0;
     while(highCheck < height && !broken){   //on the ladder and no broken devices
       highCheck += step;
-      if (highCheck > height)     //in cases where multiple steps wont equal height
+      /* in cases where steps does not divide height */
+      if (highCheck > height)
+      {     
         highCheck = height;
-      if (highCheck > safest){
+      }
+      /* if we exceed the safety limit, we get a break, record that */
+      if (highCheck > safest)
+      {
         broken = true;
         firstBreak = highCheck;
       }
       else
+      {
         safe = highCheck; //move pointer to last checked safe rung
-
-      ++drops;
-      if (highCheck > step){
+      }
+      drops++;
+      if (highCheck > step)
+      {
         if(secondDrop == -1)
+        {
           secondDrop = highCheck;
+        }
         else if (thirdDrop == -1)
+        {
           thirdDrop = highCheck;
+        }
       }
     }
     int i = safe + 1;
@@ -55,7 +78,8 @@ public class DestructiveTest {
       ++i;
     }
 
-    ret.add(2, safe);     //fill out answer array
+    /* fill out answer array */
+    ret.add(2, safe);     
     ret.add(3, drops);
     ret.add(4, step);
     ret.add(5, secondDrop);
